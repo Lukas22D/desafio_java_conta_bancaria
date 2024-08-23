ddc108081b5ca5a913a17a90045e39476edec0b7
Lucas Silva Rocha <luk.look.s.rocha@gmail.com>
2024-08-23 12:21:08 -0300

    envio ao repositório

---
+++ src/App.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class App {
+    /**
+     * Método principal da aplicação
+     * @param args
+     * @throws Exception
+     * 
+     * <p>
+     * A classe App é utilizada para representar a aplicação principal
+     *   Nela, é possível criar uma conta bancária, depositar e sacar valores
+     *  Para isso, é necessário informar o número da conta, o nome do titular, o CPF do titular e o saldo inicial
+     * </p>
+     * 
+     */
+    public static void main(String[] args) throws Exception {
+        var scanner = new Scanner(System.in);
+        var conta = new ContaBancaria();
+        System.out.println("Bem-vindo ao Banco do Brasil \n \n Escollha uma opção: \n 1 - Criar conta \n 2 - Sacar \n 3 - Depositar \n 4 - Sair");
+        var opcao = 0;
+         do{
+             opcao = scanner.nextInt();
+            switch(opcao){
+                case 1:
+                    System.out.println("Digite o número da conta: ");
+                    var numero_conta = scanner.next();
+                    System.out.println("Digite o nome do titular: ");
+                    var nome_titular = scanner.next();
+                    System.out.println("Digite o CPF do titular: ");
+                    var cpf_titular = scanner.next();
+                    System.out.println("Digite o saldo inicial: ");
+                    var saldo = scanner.nextDouble();
+                    conta.criar_conta(numero_conta, nome_titular, cpf_titular, saldo);
+                  
+                    break;
+                case 2:
+                    if(conta.cpf_titular == null){
+                        System.out.println("Conta não criada!");
+                        break;
+                    }else{
+                        System.out.println("Digite o valor a ser sacado: ");
+                        var valor = scanner.nextDouble();
+                        conta.sacar(valor);
+                        
+                    }
+                    break;
+                case 3:
+                if(conta.cpf_titular == null){
+                    System.out.println("Conta não criada!");
+                    break;
+                }else{
+                    System.out.println("Digite o valor a ser depositado: ");
+                    var valor = scanner.nextDouble();
+                    conta.depositar(valor);
+                 
+                    break;
+                }
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+                    
+            }
+
+
+
+
+
+         }while(opcao != 4);
+         scanner.close();
+    }
+}
---
+++ src/ContaBancaria.java
@@ -0,0 +1,74 @@
+/**
+ *  <h1>Conta Bancária</h1>
+ *
+ * <p>
+ * A classe ContaBancaria é utilizada para representar uma conta bancária
+ * Para isso, ela possui os seguintes atributos:
+ * <ul>
+ * <li>@param numero_conta: inteiro que representa o número da conta</li>
+ * <li>@param nome_titular: string que representa o nome do titular da conta</li>
+ * <li>@param cpf_titular: string que representa o CPF do titular da conta</li>
+ * <li>@param saldo: double que representa o saldo da conta</li>
+ * </ul>
+ * </p>
+ */
+
+public class ContaBancaria {
+    public String numero_conta;
+    public String nome_titular;
+    public String cpf_titular;
+    public double saldo;
+
+    /**
+     * Construtor da classe ContaBancaria
+     * Inicializa os atributos da classe com valores padrão
+     */
+    public ContaBancaria() {
+        this.numero_conta = null;
+        this.nome_titular = null;
+        this.cpf_titular = null;
+        this.saldo = 0;
+    }
+    /**
+     * Método que cria uma conta bancária
+     * @param numero_conta
+     * @param nome_titular
+     * @param cpf_titular
+     * @param saldo
+     */
+
+    public void criar_conta(String numero_conta, String nome_titular, String cpf_titular, double saldo) {
+        this.numero_conta = numero_conta;
+        this.nome_titular = nome_titular;
+        this.cpf_titular = cpf_titular;
+        this.saldo = saldo;
+    }
+
+    /**
+     * Método que realiza um depósito na conta bancária
+     * 
+     * @param valor: double que representa o valor a ser depositado
+     */
+    public void depositar(double valor) {
+            this.saldo += valor;
+            System.out.println("Depósito realizado com sucesso! Novo saldo: " + this.saldo);
+        
+    }
+
+    /**
+     * Método que realiza um saque na conta bancária
+     * 
+     * @param valor: double que representa o valor a ser sacado
+     */
+    public void sacar(double valor) {
+        if(this.saldo - valor < 0){
+            System.out.println("Saldo insuficiente!");
+        }else{
+            this.saldo -= valor;
+            System.out.println("Saque realizado com sucesso! Novo saldo: " + this.saldo);
+        }
+    }
+
+
+
+}
